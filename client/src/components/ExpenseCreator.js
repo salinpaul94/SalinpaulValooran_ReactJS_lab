c00940d49a079030a25151ad61efa27424a81456
salin paul <salin.paul@geckolyst.com>
2024-02-13 19:09:53 +0530

    updating form for expense creation

--- client/src/components/ExpenseCreator.js
+++ client/src/components/ExpenseCreator.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 const ExpenseCreator = () => {
 
@@ -9,26 +10,58 @@ const ExpenseCreator = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+	function expenseForm() {
+		return (
+			<Form>
+				<Form.Group className="mb-3" controlId="expenseDescription">
+					<Form.Label>Expense Description</Form.Label>
+					<Form.Control type="text" placeholder="Enter expense description" />
+				</Form.Group>
+	
+				<Form.Group className="mb-3" controlId="payeeName">
+					<Form.Label>Payee Name</Form.Label>
+					<Form.Select aria-label="Default select example">
+      			<option>Open this select menu</option>
+      			<option value="1">One</option>
+      			<option value="2">Two</option>
+      			<option value="3">Three</option>
+    			</Form.Select>
+				</Form.Group>
+
+				<Form.Group className="mb-3" controlId="price">
+					<Form.Label>Price</Form.Label>
+					<Form.Control type="number" placeholder="Enter expense Amount" />
+				</Form.Group>
+
+				<Form.Group className="mb-3" controlId="expenseDate">
+					<Form.Label>Expense Description</Form.Label>
+					<Form.Control type="date"/>
+				</Form.Group>
+
+				<Button variant="secondary" onClick={handleClose}>
+            Close
+        </Button>
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		);
+	}
+
   return (
 
 		<div>
 			<Button variant="primary" onClick={handleShow} className='float-end'>
-        Launch demo modal
+			Add new Expenditure
       </Button>
 
 			<Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Expense Details</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+					{expenseForm()}
+				</Modal.Body>
       </Modal>
 		</div>
   )
